a22f95b04d7ac0d9dc41e004194cfe5f4f929299
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-16 15:02:40 +0300

    questions manipulation

--- src/app/components/admin/all-questions/all-questions.component.ts
+++ src/app/components/admin/all-questions/all-questions.component.ts
@@ -1,10 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Question } from 'src/app/interfaces/question.interface';
+import { AdminSingleQuestionComponent } from './admin-single-question/admin-single-question.component';
 
 @Component({
   selector: 'app-all-questions',
+  standalone: true,
+  imports: [CommonModule, AdminSingleQuestionComponent],
   templateUrl: './all-questions.component.html',
   styleUrls: ['./all-questions.component.css']
 })
 export class AllQuestionsComponent {
+  questions: Question[] = [];
 
+  constructor(private store: Store<AppState>){
+    
+  }
 }
