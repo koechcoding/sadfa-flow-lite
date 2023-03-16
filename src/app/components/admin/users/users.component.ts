c08405b9ecd9393c3ffec87bcdbe952b726f2ba7
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-16 15:49:13 +0300

    single-user

--- src/app/components/admin/users/users.component.ts
+++ src/app/components/admin/users/users.component.ts
@@ -1,10 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { User } from 'src/app/interfaces/user.interface';
+import { SingleUserComponent } from './single-user/single-user.component';
 
 @Component({
   selector: 'app-users',
+  standalone: true,
+  imports: [CommonModule, SingleUserComponent],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  users: User [] = [];
 
+  constructor(private store: Store<AppState>){
+
+    this.store.select(selectUsers).subscribe((users)=>{
+      this.users = users;
+      console.log(this.users);
+      
+    })
+  }
 }
