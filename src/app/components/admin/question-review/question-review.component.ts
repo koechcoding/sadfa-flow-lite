c2b8baf02b710d0f8bb2717b85a333a520b0ec52
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-16 21:03:54 +0300

    question review

---
+++ src/app/components/admin/question-review/question-review.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-review',
+  templateUrl: './question-review.component.html',
+  styleUrls: ['./question-review.component.css']
+})
+export class QuestionReviewComponent {
+
+}
