67071dba01dbdf19d1d401e3b730553f8172951a
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-17 14:10:45 +0300

    privacy policy

--- src/app/components/home/privacy/privacy.component.ts
+++ src/app/components/home/privacy/privacy.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-privacy',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './privacy.component.html',
   styleUrls: ['./privacy.component.css']
 })
