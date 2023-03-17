215d790a7a787369086def1527f50fcac28cf9e5
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-17 15:03:49 +0300

    questions

--- src/app/components/home/questions/single-question/single-question.component.ts
+++ src/app/components/home/questions/single-question/single-question.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Question } from 'src/app/interfaces/question.interface';
+import { RouterModule } from '@angular/router';
+import { QuestionShortenerPipe } from 'src/app/pipes/question-shortener/question-shortener.pipe';
 
 @Component({
   selector: 'app-single-question',
+  standalone: true,
+  imports: [CommonModule, QuestionShortenerPipe, RouterModule],
   templateUrl: './single-question.component.html',
   styleUrls: ['./single-question.component.css']
 })
 export class SingleQuestionComponent {
+
+  @Input() question: Question | undefined;
+
+  constructor() { }
+
+
 
 }
