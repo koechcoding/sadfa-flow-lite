01ad49e1e44f7a7eca638fbc871148d8524f9350
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-17 14:59:12 +0300

    questions

---
+++ src/app/components/home/questions/single-question/single-question.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-question',
+  templateUrl: './single-question.component.html',
+  styleUrls: ['./single-question.component.css']
+})
+export class SingleQuestionComponent {
+
+}
