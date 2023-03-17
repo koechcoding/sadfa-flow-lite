9a99980fd05473a7309e5b3fc76c4b88d37dc0d8
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-18 01:23:01 +0300

    length of question

--- src/app/pipes/shorten-question.pipe.ts
+++ src/app/pipes/shorten-question.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'shortenQuestion'
+  name: 'shortenQuestion',
+  standalone: true
 })
 export class ShortenQuestionPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(question: string, length: number): string {
+    //taking in questions and length
+
+    if(question.length<=length){
+      return question
+    }
+
+    //question longer than length
+
+    return question.slice(0, length) + '...';
   }
 
 }
