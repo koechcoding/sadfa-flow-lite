d9323d007f24d741269b6f9cadb6927428b7413d
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-18 01:36:53 +0300

    shorten username

--- src/app/pipes/shorten-username.pipe.ts
+++ src/app/pipes/shorten-username.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'shortenUsername'
+  name: 'shortenUsername',
+  standalone: true
 })
 export class ShortenUsernamePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(user: string, length: number): any {
+    // user being shorter than length
+
+    if(user.length<=length){
+      return user;
+    }
+
+    //if user is longer than length
+
+    return user.slice(0, length) + '...';
   }
 
 }
