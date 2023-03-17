682447ec5976f9f953cc6fcf528392f2e8704ebb
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-17 14:48:31 +0300

    user profile

--- src/app/components/home/profile/profile.component.ts
+++ src/app/components/home/profile/profile.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { User } from 'src/app/interfaces/user.interface';
+import { loggedInUser } from 'src/app/interfaces/loggedInUser.interface';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.state';
+import { selectLoggedInUser } from 'src/app/state/selectors/loggedInUser.selector';
 
 @Component({
   selector: 'app-profile',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  user: User = {
+    id: ' ',
+    name: 'koechcoding ',
+    email: 'koechcoding@gmail.com',
+    created_at: '2023-10-10 12:00:00',
+    updated_at: '2023-10-10 12:00:00'
+  }
+  loggedInUser!: loggedInUser 
+
+  constructor(private store: Store<AppState>) { }
 
 }
