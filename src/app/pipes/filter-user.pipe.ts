c1dda9cc59375c2e2c88a18f218d601472bd9f50
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-18 01:29:13 +0300

    filter users

--- src/app/pipes/filter-user.pipe.ts
+++ src/app/pipes/filter-user.pipe.ts
@@ -1,12 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filterUser'
+  name: 'filterUser',
+  standalone: true
 })
 export class FilterUserPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(users:any, search: string): any {
+    
+    //if no search string, return all users
+    if(!search){
+      return users
+    }
+
+    //filter users if search string
+    return users.filter((user: any)=>{
+      return user.name.toLowerCase().includes(search.toLowerCase());
+    })
   }
 
 }
