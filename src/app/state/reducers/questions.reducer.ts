d7839ce4b08fb68673d5def52d296bdb557e4d60
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-18 22:46:57 +0300

    question reducer

---
+++ src/app/state/reducers/questions.reducer.ts
@@ -0,0 +1,55 @@
+import { createReducer,on } from "@ngrx/store";
+import * as QuestionsActions from '../actions/questions.actions';
+
+import { Question } from "src/app/interfaces/question.interface";
+import { QuestionVote } from "src/app/interfaces/questionVote.interface";
+
+export interface QuestionsState {
+    questions: Question[];
+    loading: boolean;
+    error: any;
+}
+
+
+export const initialState: QuestionsState = {
+    questions: [],
+    loading: false,
+    error: null
+};
+
+
+export const questionsReducer = createReducer(
+    initialState,
+    on(QuestionsActions.loadQuestions, (state) => {
+        return {
+            ...state,
+            loading: true
+        }
+    }
+    ),
+    on(QuestionsActions.loadQuestionsSuccess, (state, { questions }) => {
+        return {
+            ...state,
+            loading: false,
+            questions: [...questions]
+        }
+    }
+    ),
+    on(QuestionsActions.loadQuestionsFailure, (state, { error }) => {
+        return {
+            ...state,
+            loading: false,
+            error
+        }
+    }
+    ),
+    on(QuestionsActions.addQuestion, (state, { question }) => {
+        return {
+            ...state,
+            questions: [...state.questions, question]
+        }
+    }
+    ),
+    
+
+);
