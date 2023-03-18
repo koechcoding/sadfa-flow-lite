dff8d7836f1b1a3941b5f7a561e456c2535b6ce6
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-19 02:21:34 +0300

    directives

--- src/app/directives/background-color/background-color.directive.ts
+++ src/app/directives/background-color/background-color.directive.ts
@@ -1,10 +1,16 @@
 import { Directive } from '@angular/core';
+import { Input, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appBackgroundColor]'
+  selector: '[appBackgroundColor]',
+  standalone: true
 })
 export class BackgroundColorDirective {
 
-  constructor() { }
+   @Input() appBackgroundColor: string = 'red';
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', this.appBackgroundColor);
+  }
 
 }
