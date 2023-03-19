595fc44df254888bf2244ab0f2e339ef257b15ff
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-19 03:24:28 +0300

    single org

--- src/app/components/organizations/single-org/single-org.component.ts
+++ src/app/components/organizations/single-org/single-org.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Company } from 'src/app/interfaces/company.interface';
+import { Company } from 'src/app/interfaces/org.interface';
 
 @Component({
   selector: 'app-single-company',
