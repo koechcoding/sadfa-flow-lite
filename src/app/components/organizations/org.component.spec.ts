ad77a279a6d25f462666a1ac52554957cf59a888
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-19 03:28:26 +0300

    org

--- src/app/components/organizations/org.component.spec.ts
+++ src/app/components/organizations/org.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CompaniesComponent } from './companies.component';
+import { CompaniesComponent } from './org.component';
 
 describe('CompaniesComponent', () => {
   let component: CompaniesComponent;
