1ba939a94cd616cc8955e3746a1534fd34954da7
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-25 21:45:47 +0300

    recap

--- src/app/services/register/register.service.ts
+++ src/app/services/register/register.service.ts
@@ -5,23 +5,18 @@ import { AppState } from 'src/app/app.state';
 import { loggedInUser } from 'src/app/interfaces/loggedInUser.interface';
 import * as registerUserActions from 'src/app/state/actions/registerUser.actions'
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { userToRegister } from 'src/app/interfaces/register.interface';
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor(private store: Store<AppState>, private router: Router) { }
+  constructor(private store: Store<AppState>, private router: Router, private http: HttpClient) { }
 
 
   registerUser(user: userToRegister) {
-    return new Observable<loggedInUser>(observer => {
-
-      setTimeout(() => {
-        this.store.dispatch(registerUserActions.register({ user }));
-        observer.next({ id: '1', username: 'test', email: 'test@gmail.com', token: 'test' }); 
-      },4000);
-    });
+    return this.http.post('http://localhost:4000/api/users/register', user);
   }
 
 
