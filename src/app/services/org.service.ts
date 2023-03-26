4547533fcfc8179ec1e8794616ca1b50dbdb0c9d
Kelvin Koech <koechkelvin2001@gmail.com>
2023-03-26 11:56:30 +0300

    services

---
+++ src/app/services/org.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Company } from '../interfaces/org.interface';
+import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.state';
+import { selectTags } from '../state/selectors/tag.selector';
+import { firstValueFrom } from 'rxjs';
+import { Tag } from '../interfaces/abouts.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+ 
+  companies: Company[] = []
+  constructor(private http: HttpClient,private store: Store<AppState>) { }
+
+
+  getCompanies(): Observable<Company[]> {
+
+    // include tag in company and return companies as observable
+
+    let tags: Tag[] = [];
+    this.store.select(selectTags).subscribe((data) => {
+      tags = data;
+    }
+    )
+    // add tag to company
+    return this.http.get<Company[]>('http://localhost:4000/api/companies').pipe(
+      tap((data)=>{
+        data.forEach((company)=>{
+          company.tag = tags.find((tag)=>tag.id === company.tag_id) as Tag;
+        })
+      })
+    )
+
+  
+  }
+
+
+}
